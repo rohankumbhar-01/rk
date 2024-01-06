bead9dc3a3e6712a7b000e8687552150d741e0da
Rohan Kumbhar <rkrohankumbhar@gmail.com>
2024-01-06 13:06:28 +0530

    added pyserial

---
+++ rk/rohan/doctype/test/test.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Rohan and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("test", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ rk/rohan/doctype/test/test.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Rohan and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+import serial
+
+class test(Document):
+	pass
